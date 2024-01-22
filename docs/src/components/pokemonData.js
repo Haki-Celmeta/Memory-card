490dcaa9af16501bcc6782ca3221edfdf22264fd
Haki Celmeta <celmeta.haki@gmail.com>
2024-01-22 20:17:05 +0100

    Favicon and docs folder

---
+++ docs/src/components/pokemonData.js
@@ -0,0 +1,7 @@
+const pokemons = [
+  'charmander', 'charizard', 'squirtle', 'caterpie', 'butterfree', 'pidgey', 'rattata',
+  'arbok', 'ekans', 'pikachu', 'sandshrew', 'nidorina', 'nidoking', 'zubat',
+  'paras', 'diglett', 'dugtrio', 'ponyta', 'seel', 'gengar', 'krabby'
+]
+
+export default pokemons;
