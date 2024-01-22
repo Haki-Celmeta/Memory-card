57616b52122a87d24f74d4bebc098a8bb3bbd9bc
Haki Celmeta <celmeta.haki@gmail.com>
2024-01-22 20:58:44 +0100

    deploy

--- docs/src/components/pokemonData.js
+++
@@ -1,7 +0,0 @@
-const pokemons = [
-  'charmander', 'charizard', 'squirtle', 'caterpie', 'butterfree', 'pidgey', 'rattata',
-  'arbok', 'ekans', 'pikachu', 'sandshrew', 'nidorina', 'nidoking', 'zubat',
-  'paras', 'diglett', 'dugtrio', 'ponyta', 'seel', 'gengar', 'krabby'
-]
-
-export default pokemons;
